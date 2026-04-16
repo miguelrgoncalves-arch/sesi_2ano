53f98d8ba882c7f7f321bc4d5acfed1d07f4986b
miguelrgoncalves-arch <miguel.r.goncalves@edu.senai.br>
2026-04-16 10:08:09 -0300

    v3 - Página principal da aplicação DevApp
    Adicionado o nome do curso do desenvolvedor

--- main.php
+++ main.php
@@ -1,7 +1,8 @@
 <?php 
 
 $dev = "Olá mundo!";
+$nome = "Eu sou o Miguel!";
+$descricao = "Curso: Técnico em Desenvolvimento de Sistemas.";
+echo $dev. " ". $nome. "\n". $descricao 
 
-echo $dev;
-$nome = "Eu sou o Miguel!"
 ?>
